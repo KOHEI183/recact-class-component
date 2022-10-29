43ec90caedc9e9bce63661641d6f849e603c2f14
KOHEI183 <zbtbfuufp@gmail.com>
2022-10-29 17:14:44 +0900

    fix:複数の値を更新

--- src/components/increment/increments.js
+++ src/components/increment/increments.js
@@ -6,23 +6,76 @@ class Increments extends React.Component {
     console.debug("class Increments");
     super(props);
     // 子クラスのconstructor()ではsuper()の前でthisを参照することはできない
-    this.state = { count: 0 };
+    this.state = { wait: 1, count: 0, age: 0 };
   }
   // 関数
-  increment = (targetNum) =>
+  countIncrement = (targetNum) =>
     this.setState({ count: this.state.count + targetNum });
-  decrement = (targetNum) =>
+  countDecrement = (targetNum) =>
     this.setState({ count: this.state.count - targetNum });
+
+  ageIncrement = (targetNum) =>
+    this.setState((prev) => {
+      console.log("prevを用いてageを更新");
+      return {
+        age: prev.age + targetNum,
+      };
+    });
+  ageDecrement = (targetNum) =>
+    this.setState((prev) => {
+      console.log("prevを用いてageを更新");
+      return {
+        age: prev.age - targetNum,
+      };
+    });
+
+  allIncrement = (targetNum) =>
+    this.setState((prev) => {
+      console.log("複数の値を更新");
+      return {
+        count: prev.count + targetNum,
+        age: prev.age + targetNum,
+      };
+    });
+  allDecrement = (targetNum) =>
+    this.setState((prev) => {
+      console.log("複数の値を更新");
+      return {
+        count: prev.count - targetNum,
+        age: prev.age - targetNum,
+      };
+    });
 
   render() {
     return (
       <>
         <p>{this.props.pageTitle}</p>
-        <p>count : {this.state.count}</p>
         <div>
+          <p>count : {this.state.count}</p>
           {/* アロー関数にしないと無限描画する */}
-          <button onClick={() => this.increment(1)}>increment</button>
-          <button onClick={() => this.decrement(1)}>decrement</button>
+          <button onClick={() => this.countIncrement(this.state.wait)}>
+            count increment
+          </button>
+          <button onClick={() => this.countDecrement(this.state.wait)}>
+            count decrement
+          </button>
+        </div>
+        <div>
+          <p>count : {this.state.age}</p>
+          <button onClick={() => this.ageIncrement(this.state.wait)}>
+            age increment
+          </button>
+          <button onClick={() => this.ageDecrement(this.state.wait)}>
+            age decrement
+          </button>
+        </div>
+        <div>
+          <button onClick={() => this.allIncrement(this.state.wait)}>
+            all increment
+          </button>
+          <button onClick={() => this.allDecrement(this.state.wait)}>
+            all decrement
+          </button>
         </div>
       </>
     );
