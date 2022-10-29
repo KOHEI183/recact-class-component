7df9aadbcf6a589ff98ba36ced0b822d67998707
KOHEI183 <zbtbfuufp@gmail.com>
2022-10-29 15:25:03 +0900

    fix:class componentに置き換え

---
+++ src/Root.js
@@ -0,0 +1,34 @@
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+
+// class component名 extends React.Component
+// React.Component サブクラスにはrenderメソッドを定義しなければならない
+class Root extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+export default Root;
