bd5c29b3b0ecc559c6af52fe8a7df1ae9894fc18
KOHEI183 <zbtbfuufp@gmail.com>
2022-10-29 17:22:55 +0900

    fix:lintの修正

--- src/components/lifeCycle/lifeCycle.js
+++ src/components/lifeCycle/lifeCycle.js
@@ -1,3 +1,4 @@
+/* eslint-disable valid-typeof */
 import React from "react";
 
 class LifeCycle extends React.Component {
@@ -6,16 +7,26 @@ class LifeCycle extends React.Component {
     super();
     this.state = {
       users: [],
+      date: new Date(),
     };
   }
 
   componentDidMount() {
     // コンポーネントがマウント(配置)される直前に呼び出されるメソッド。
+
+    // userを取得してみてライフサイクルではどうなるのか見る
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((usersList) => {
         this.setState({ users: usersList });
       });
+
+    // componentDidUpdateを試してみる
+    const timer = setInterval(() => {
+      this.setState({ date: new Date() });
+    }, 1000);
+    this.setState({ timer });
+
     console.debug(
       "componentDidMount",
       "コンポーネントのマウント前",
@@ -30,6 +41,7 @@ class LifeCycle extends React.Component {
     );
   }
   componentWillUnmount() {
+    clearInterval(this.state.timer);
     console.debug(
       "componentWillUnmount",
       "コンポーネントがDOMから削除されるとき",
@@ -61,6 +73,7 @@ class LifeCycle extends React.Component {
     return (
       <>
         <p>{this.props.pageTitle}</p>
+        <p>{this.state.date.toString()}</p>
         {this.state.users.map((user) => (
           <p key={user.id}>{user.name}</p>
         ))}
